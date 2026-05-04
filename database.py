b141b9902636ea315a6ff2dbe72b1792cfdc306b
Sagar Paschapur <sagar.p@bigcity.in>
2026-05-04 14:18:48 +0530

    added the error message

--- database.py
+++ database.py
@@ -3,6 +3,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+# 🔴 Safety check
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set in environment variables")
+
 print("DATABASE_URL:", DATABASE_URL)
 
 engine = create_engine(
