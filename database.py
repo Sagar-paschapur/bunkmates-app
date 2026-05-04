365ae80a911c27872e1209881fcf01e349c5484a
Sagar Paschapur <sagar.p@bigcity.in>
2026-05-04 13:08:02 +0530

    added the database url print

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+print("DATABASE_URL:", DATABASE_URL)
 
 engine = create_engine(
     DATABASE_URL,
